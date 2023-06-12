050a40563334c7ff5271eae46875360738470358
Andriy Popovych <viktpopo@gmail.com>
2023-06-12 09:24:37 +0300

    12060924

---
+++ js/M3/autoperevirka.js
@@ -0,0 +1,105 @@
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// Change code below this line
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[apartment.tags.length - 1];
+// Change code above this line
+
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ["premium", "promoted", "top", "trusted"],
+//   owner: {
+//     name: "Henry Sibola",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {
+//   country: 'Jamaica',
+//   city : 'Kingston'
+// }
+
+// console.log(apartment);
+
+
+// const emailInputName = "email";
+// const passwordInputName = "password";
+
+// const credentials = {
+//   // Change code below this line
+
+//   [emailInputName]: 'henry.carter@aptmail.com',
+//   [passwordInputName] : 'jqueryismyjam',
+
+//   // Change code above this line
+// };
+// console.log(credentials);
+
+
+
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   rating: 8.38,
+// };
+
+// for (const key in book) {
+//   // Ключ
+//   // console.log(key);
+//   // Значення властивості з таким ключем
+//   console.log(`${key} text ${book[key]}`);
+// }
+
+
+
+
+
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Change code below this line
+// for (key in apartment) {
+// keys.push(key);
+//   values.push(apartment[key]); 
+// }
+
+
+const animal = {
+  legs: 4,
+};
+const dog = Object.create(animal);
+dog.name = "Mango";
+
+console.log(dog); // {name: 'Mango'}
+console.log(dog.name); // 'Mango'
+console.log(dog.legs); // 4
---
+++ js/M3/masyvobjektiv.js
@@ -0,0 +1,13 @@
+const rodyna = [
+    { name: 'Andriy', online: false, doma: true, },
+    { name: 'Viktoria', online: true, doma: true, },
+    { name: 'Anna Szofia', online: false, doma: false, },
+    { name: 'Viktor', online: true, doma: false, },
+    { name: 'Anna', online: false, doma: true, },
+    { name: 'Myron', online: true, doma: true, },
+    ]
+
+console.table(rodyna);
+
+
+
